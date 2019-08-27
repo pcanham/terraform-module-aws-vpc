20383badda14a0aa126c59b91072d21532934e3c
Paul Canham <paul@canham.io>
2019-08-27 11:55:12 +0100

    Fixing routing table naming

--- routing-tables.tf
+++ routing-tables.tf
@@ -23,7 +23,34 @@ resource "aws_route_table" "public" {
 
 }
 
-resource "aws_route_table" "private" {
+resource "aws_route_table" "private01" {
+  vpc_id = aws_vpc.pro.id
+  count  = length(var.availability_zones)
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = var.nat_gateway ? element(concat(aws_nat_gateway.nat_gw.*.id, list("")), count.index) : aws_internet_gateway.internet_gw.id
+    #gateway_id = "${length(aws_nat_gateway.nat_gw.*.id) > 0 ? element(concat(aws_nat_gateway.nat_gw.*.id, list("")), count.index) : aws_internet_gateway.internet_gw.id}"
+    #gateway_id = "${aws_internet_gateway.internet_gw.id}"
+  }
+
+  tags = merge(
+    local.common_tags,
+    { "Name" = lower(
+      format(
+        "rtprv%02d%s-%s-%s",
+        count.index + 1,
+        substr(element(var.availability_zones, count.index), -1, 1),
+        var.project_tag,
+        var.environment_tag,
+      ),
+      )
+    }
+    #    { "Name" = "${lower(format("rtprv%02d%s-%s-%s", 98, substr(element(var.availability_zones,count.index), -1, 1), var.project_tag, var.environment_tag))}" }
+  )
+}
+
+resource "aws_route_table" "private02" {
   vpc_id = aws_vpc.pro.id
   count  = length(var.availability_zones)
 
@@ -60,11 +87,11 @@ resource "aws_route_table_association" "public" {
 resource "aws_route_table_association" "private01" {
   count          = length(var.private_cidr_blocks01)
   subnet_id      = element(aws_subnet.pm_pro_private01.*.id, count.index)
-  route_table_id = element(aws_route_table.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private01.*.id, count.index)
 }
 
 resource "aws_route_table_association" "private02" {
   count          = length(var.private_cidr_blocks02)
   subnet_id      = element(aws_subnet.pm_pro_private02.*.id, count.index)
-  route_table_id = element(aws_route_table.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private02.*.id, count.index)
 }
