70c2b400875c187b480dc37f2ef0155c97319652
Paul Canham <paul@canham.io>
2019-09-23 14:08:33 +0100

    missed a required count statement on public subnets

--- vpc-endpoint.tf
+++ vpc-endpoint.tf
@@ -11,7 +11,7 @@ resource "aws_vpc_endpoint" "s3" {
 resource "aws_vpc_endpoint_route_table_association" "public_s3" {
   count          = length(var.public_cidr_blocks)
   vpc_endpoint_id = aws_vpc_endpoint.s3.id
-  route_table_id  = aws_route_table.public.*.id
+  route_table_id  = element(aws_route_table.public.*.id, count.index)
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private01_s3" {
