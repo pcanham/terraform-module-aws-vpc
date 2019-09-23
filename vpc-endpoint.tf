b8b92d490289a2d4db079004373e073faaff3936
Paul Canham <paul@canham.io>
2019-09-23 14:05:38 +0100

    fix: enabling s3 vpc endpoint
    this was being created but not linked to routing tables.

--- vpc-endpoint.tf
+++ vpc-endpoint.tf
@@ -7,3 +7,27 @@ resource "aws_vpc_endpoint" "s3" {
   )
   tags = var.tags
 }
+
+resource "aws_vpc_endpoint_route_table_association" "public_s3" {
+  count          = length(var.public_cidr_blocks)
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id  = aws_route_table.public.*.id
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private01_s3" {
+  count           = length(var.private_cidr_blocks01)
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id  = element(aws_route_table.private01.*.id, count.index)
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private02_s3" {
+  count           = length(var.private_cidr_blocks02)
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id  = element(aws_route_table.private02.*.id, count.index)
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private03_s3" {
+  count           = length(var.private_cidr_blocks03)
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id  = element(aws_route_table.private03.*.id, count.index)
+}
