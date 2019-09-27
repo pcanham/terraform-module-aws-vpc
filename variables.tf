177ad4084ef21d228341d3d550bf511d625ed093
Paul Canham <paul@canham.io>
2019-09-27 10:00:24 +0100

    feat: set default to 1 nat gw per az

--- variables.tf
+++ variables.tf
@@ -33,7 +33,7 @@ variable "single_nat_gateway" {
 variable "one_nat_gateway_per_az" {
   description = "Should be true if you want one NAT Gateway per availability zone."
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "public_cidr_blocks" {
