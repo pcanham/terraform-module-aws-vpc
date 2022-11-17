72e5d6fb2c63c6921decafe54b754db3193e04ec
Paul Canham <paul@canham.io>
2022-11-17 11:22:36 +0000

    deprecated: List items should be accessed using square brackets

--- vpc-endpoint.tf
+++ vpc-endpoint.tf
@@ -22,23 +22,23 @@ resource "aws_vpc_endpoint" "s3" {
 resource "aws_vpc_endpoint_route_table_association" "public_s3" {
   count           = var.enable_vpc_s3_endpoint && length(var.public_cidr_blocks) > 0 ? 1 : 0
   vpc_endpoint_id = aws_vpc_endpoint.s3[0].id
-  route_table_id  = element(aws_route_table.public.*.id, count.index)
+  route_table_id  = element(aws_route_table.public[*].id, count.index)
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private01_s3" {
   count           = var.enable_vpc_s3_endpoint && length(var.private_cidr_blocks01) > 0 ? 1 : 0
   vpc_endpoint_id = aws_vpc_endpoint.s3[0].id
-  route_table_id  = element(aws_route_table.private01.*.id, count.index)
+  route_table_id  = element(aws_route_table.private01[*].id, count.index)
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private02_s3" {
   count           = var.enable_vpc_s3_endpoint && length(var.private_cidr_blocks02) > 0 ? 1 : 0
   vpc_endpoint_id = aws_vpc_endpoint.s3[0].id
-  route_table_id  = element(aws_route_table.private02.*.id, count.index)
+  route_table_id  = element(aws_route_table.private02[*].id, count.index)
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private03_s3" {
   count           = var.enable_vpc_s3_endpoint && length(var.private_cidr_blocks03) > 0 ? 1 : 0
   vpc_endpoint_id = aws_vpc_endpoint.s3[0].id
-  route_table_id  = element(aws_route_table.private03.*.id, count.index)
+  route_table_id  = element(aws_route_table.private03[*].id, count.index)
 }
