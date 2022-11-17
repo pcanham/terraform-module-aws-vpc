74fbda9b3bfbb0d3049cb6e1e032e78430f2df30
Paul Canham <paul@canham.io>
2022-11-17 14:43:22 +0000

    cleaning up IAM permissions format

--- vpc-flowlogs.tf
+++ vpc-flowlogs.tf
@@ -30,7 +30,7 @@ resource "aws_flow_log" "VpcFlowLog-Reject" {
 
   log_destination_type = "cloud-watch-logs"
   log_destination      = aws_cloudwatch_log_group.LogGroup-Reject.arn
-  iam_role_arn         = aws_iam_role.VpCFlowLogRole.arn
+  iam_role_arn         = aws_iam_role.vpc_flow_log_cloudwatch.arn
   vpc_id               = aws_vpc.pro.id
   traffic_type         = "REJECT"
   tags                 = var.tags
@@ -40,13 +40,26 @@ resource "aws_flow_log" "VpcFlowLog-Accept" {
 
   log_destination_type = "cloud-watch-logs"
   log_destination      = aws_cloudwatch_log_group.LogGroup-Accept.arn
-  iam_role_arn         = aws_iam_role.VpCFlowLogRole.arn
+  iam_role_arn         = aws_iam_role.vpc_flow_log_cloudwatch.arn
   vpc_id               = aws_vpc.pro.id
   traffic_type         = "ACCEPT"
   tags                 = var.tags
 }
 
-resource "aws_iam_role" "VpCFlowLogRole" {
+# Create Assume role permissions
+data "aws_iam_policy_document" "vpc_flow_logs_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+  }
+}
+
+# Create role and assign the assume role permissions
+resource "aws_iam_role" "vpc_flow_log_cloudwatch" {
   name_prefix = lower(
     format(
       "VpCFlowLog-%s-%s",
@@ -54,50 +67,39 @@ resource "aws_iam_role" "VpCFlowLogRole" {
       var.environment_tag,
     ),
   )
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "vpc-flow-logs.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+  path               = "/system/"
+  assume_role_policy = data.aws_iam_policy_document.vpc_flow_logs_assume_role.json
   tags               = var.tags
 }
 
-resource "aws_iam_role_policy" "VpCFlowLogPolicy" {
-  name_prefix = lower(
-    format(
-      "VpCFlowLog-%s-%s",
-      var.project_tag,
-      var.environment_tag,
-    ),
-  )
-  role = aws_iam_role.VpCFlowLogRole.id
 
-  #tfsec:ignore:aws-iam-no-policy-wildcards
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
+# Create policy document and policy
+data "aws_iam_policy_document" "vpc_flow_log_cloudwatch" {
+
+  statement {
+    sid = "VPCFlowLogs_To_CloudWatch"
+
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
     ]
-  })
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "vpc_flow_log_cloudwatch" {
+  name_prefix = "vpc-flow-log-to-cloudwatch-"
+  policy      = data.aws_iam_policy_document.vpc_flow_log_cloudwatch.json
+  tags        = var.tags
+}
+
+# Attach the Policy to role
+resource "aws_iam_role_policy_attachment" "vpc_flow_log_cloudwatch" {
+  role       = aws_iam_role.vpc_flow_log_cloudwatch.name
+  policy_arn = aws_iam_policy.vpc_flow_log_cloudwatch.arn
 }
